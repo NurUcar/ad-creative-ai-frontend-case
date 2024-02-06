d4438ec19e08bac9bd94c15b760b7595c516808d
nurucar <nur0ucar@gmail.com>
2024-02-06 19:16:35 +0300

    chore:inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        secondaryMadderLake: "#D33737",
+        primaryBlue: "#4CA7D4",
+        secondaryFadingSunset: "#B5B5C3",
+        maximumBlue: "#4CA7D4",
+      },
+    },
+  },
+  plugins: [],
+};
